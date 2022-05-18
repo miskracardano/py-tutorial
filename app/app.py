fbf70fc53c44a192162699cee0a1b2dd9bcf9da3
Marko Iskra <m.iskra@cardano.com>
2022-05-18 17:00:38 +0200

    Add comment for extra clarity.

--- app/app.py
+++ app/app.py
@@ -1,13 +1,16 @@
-import sys
-from pathlib import Path
+import sys                          # Keep at top to add to add root dir to PYTHONPATH.
+from pathlib import Path            # Keep at top to add to add root dir to PYTHONPATH.
 
-sys.path.append(str(Path(__file__).parent.parent))
+root = Path(__file__).parent.parent # Keep at top to add to add root dir to PYTHONPATH.
+sys.path.append(str(root))          # Keep at top to add to add root dir to PYTHONPATH.
+
 
 def main():
     """
     Main entry point of app.
     """
     pass
+
 
 if __name__ == '__main__':
     main()
