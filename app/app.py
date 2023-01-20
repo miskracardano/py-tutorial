c2eca4002fb0442fd8a58ef9208237b21557efa5
CARDANOADS\l.torn <l.torn@cardano.com>
2023-01-20 11:50:18 +0100

    First commit trial; add docs file, tried requirements, addition to app.py

--- app/app.py
+++ app/app.py
@@ -4,11 +4,23 @@ from pathlib import Path            # Keep at top to add to add root dir to PYTH
 root = Path(__file__).parent.parent # Keep at top to add to add root dir to PYTHONPATH.
 sys.path.append(str(root))          # Keep at top to add to add root dir to PYTHONPATH.
 
+import pandas as pd
+
+def load_portfolio_data(file: [str], header: [int]):
+    """
+    Load data portfolios
+    """
+    portfolios = pd.read_excel(file, header = header)
+
+    return portfolios
+
+
 
 def main():
     """
     Main entry point of app.
     """
+    load_portfolio_data('C:\Users\l.torn\PycharmProjects\py-tutorial\data\portfolios.xlsx', header = 1)
     pass
 
 
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. py-tutorial LT documentation master file, created by
+   sphinx-quickstart on Thu Jan 19 14:17:46 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to py-tutorial LT's documentation!
+==========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
