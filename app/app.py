d2a0331a547374858e3f08e5a195c9f8fe04d17e
CARDANOADS\l.torn <l.torn@cardano.com>
2023-01-24 09:31:44 +0100

    Get_risks function

--- app/app.py
+++ app/app.py
@@ -8,11 +8,14 @@ sys.path.append(str(root))  # Keep at top to add to add root dir to PYTHONPATH.
 # from app.read_data import read_cashrisks_data
 
 import pandas as pd
+from datetime import date
 from api.derivatives import get_all as get_all_der
 from api.derivatives import get_portfolio as get_portfolio_der
 from api.fx import get_all as get_all_fx
 from api.fx import get_portfolio as get_portfolio_fx
 
+
+# from .read_data import read_portfolio_data
 
 def read_portfolio_data(file: [str], header: [int]):
     """
@@ -31,22 +34,39 @@ def read_cashrisks_data(file: [str], header: [int]):
 
     return cashrisks
 
-def get_risks():
+
+def get_risks(portfolio):
     """
     Get risks for all portfolios
     """
-    show = get_portfolio_der(portfolio=get_all_der(), date=)
-    return show
+    risk_value_der = [{x: get_portfolio_der(x, date.today())['Value']} for x in (set(pd.Series(get_all_der()['Value'])) & set(portfolio.portfolio))]
+    risk_value_fx = [{x: get_portfolio_fx(x, date.today())['Value']} for x in (set(pd.Series(get_all_fx()['Value'])) & set(portfolio.portfolio))]
 
+    return risk_value_der, risk_value_fx
+
+def add_risks(portfolio, risk_value_der, risk_value_fx, cashrisks):
+    """
+    Add obtained risk info to portfolios dataframe
+    """
+    portfolio['risk'] =
+
+    return portfolio
 
 
 def main():
     """
     Main entry point of app.
     """
-    read_portfolio_data('C:/Users/l.torn/PycharmProjects/py-tutorial/data/portfolios.xlsx', header=1)
-    read_cashrisks_data('C:/Users/l.torn/PycharmProjects/py-tutorial/data/cashrisks.csv', header=1)
-    get_risks()
+    portfolio = read_portfolio_data('C:/Users/l.torn/PycharmProjects/py-tutorial/data/portfolios.xlsx', header=0)
+    cashrisks = read_cashrisks_data('C:/Users/l.torn/PycharmProjects/py-tutorial/data/cashrisks.csv', header=0)
+
+    risk_value_der, risk_value_fx = get_risks(portfolio)
+    add_risks(portfolio, risk_value_der, risk_value_fx, cashrisks)
+
+
+
+    print(portfolio, cashrisks, risk_value_der, risk_value_fx)
+
     pass
 
 
