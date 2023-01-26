8ee1e5b72d54324402d4ba16cc24e2be477f85e8
CARDANOADS\l.torn <l.torn@cardano.com>
2023-01-26 16:47:58 +0100

    Unittesting

--- tests/test_read_files.py
+++ tests/test_read_files.py
@@ -1,7 +1,9 @@
 from app.app import read_portfolio_data
 from app.app import read_cashrisks_data
+from app.app import get_risks
 from pathlib import Path
 import pandas as pd
+import sys
 
 root = Path(__file__).parent.parent  # Keep at top to add to add root dir to PYTHONPATH.
 sys.path.append(str(root))  # Keep at top to add to add root dir to PYTHONPATH.
@@ -19,11 +21,23 @@ data_port = {
     'id': [1, 2, 3, 4, 5, 6, 7, 8, 9],
     'portfolio': ['PortfolioA1', 'PortfolioA2', 'PortfolioA5', 'PortfolioA6', 'PortfolioB1', 'PortfolioB3',
                   'PortfolioC2', 'PortfolioC3', 'PortfolioC4'],
-    'client': ['ClientA', 'ClientA', 'ClientA', 'ClientA', 'ClientB', 'ClientB', 'ClientC', 'ClientC' 'ClientC'],
+    'client': ['ClientA', 'ClientA', 'ClientA', 'ClientA', 'ClientB', 'ClientB', 'ClientC', 'ClientC', 'ClientC'],
     'scope': ['CashBalance', 'FX', 'Liabilities', 'Exotics', 'CashBalance', 'Bonds', 'FixedIncome', 'Liabilities',
               'Hybrids'],
     'size': [2, 20, 100, 30, 10, 100, 100, 85, 10]
 }
+
+data_port_risks = {
+    'id': [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    'portfolio': ['PortfolioA1', 'PortfolioA2', 'PortfolioA5', 'PortfolioA6', 'PortfolioB1', 'PortfolioB3',
+                  'PortfolioC2', 'PortfolioC3', 'PortfolioC4'],
+    'client': ['ClientA', 'ClientA', 'ClientA', 'ClientA', 'ClientB', 'ClientB', 'ClientC', 'ClientC', 'ClientC'],
+    'scope': ['CashBalance', 'FX', 'Liabilities', 'Exotics', 'CashBalance', 'Bonds', 'FixedIncome', 'Liabilities',
+              'Hybrids'],
+    'size': [2, 20, 100, 30, 10, 100, 100, 85, 10],
+    'risk': [float('NaN'), 20, -200, 60, -170, 135, 130, float('NaN'), 30]
+}
+
 
 
 def test_read_cashrisks_data():
@@ -33,16 +47,6 @@ def test_read_cashrisks_data():
     assert df_expected.equals(df_1)
 
 
-def test_cashrisks_no_header():
-    data_no_header = read_cashrisks_data(cash_test, header=None)
-    data_expected = {
-        0: ['id', '1', '2'],
-        1: ['portfolio', 'PortfolioA5', 'PortfolioC3'],
-        2: ['risk', '-200', '-170']
-    }
-    assert data_no_header == data_expected
-
-
 def test_read_portfolio_data():
     df_1 = read_portfolio_data(port_test, header=0)
     df_expected = pd.DataFrame(data_port)
@@ -50,15 +54,9 @@ def test_read_portfolio_data():
     assert df_expected.equals(df_1)
 
 
-def test_portfolio_no_header():
-    data_no_header = read_portfolio_data(port_test, header=None)
-    data_expected = {
-        0: ['id', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-        1: ['portfolio', 'PortfolioA1', 'PortfolioA2', 'PortfolioA5', 'PortfolioA6', 'PortfolioB1', 'PortfolioB3',
-            'PortfolioC2', 'PortfolioC3', 'PortfolioC4'],
-        2: ['client', 'ClientA', 'ClientA', 'ClientA', 'ClientA', 'ClientB', 'ClientB', 'ClientC', 'ClientC' 'ClientC'],
-        3: ['scope', 'CashBalance', 'FX', 'Liabilities', 'Exotics', 'CashBalance', 'Bonds', 'FixedIncome',
-            'Liabilities', 'Hybrids'],
-        4: ['size', '2', '20', '100', '30', '10', '100', '100', '85', '10']
-    }
-    assert data_no_header == data_expected
+def test_get_risks():
+    test_risk_pf = get_risks(port_test, cash_test)
+    df_expected = pd.DataFrame(data_port_risks)
+    assert df_expected.equals(test_risk_pf)
+
+
